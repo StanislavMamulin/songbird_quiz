51d90f51c67a59a81cf3951c44a4652f73a524ae
Stanislav Mamulin <StanislavPM@ya.ru>
2022-11-21 12:06:25 +0500

    feat: add answer button component

---
+++ songbird/src/components/AnswerButton/index.js
@@ -0,0 +1,22 @@
+import { htmlToElement } from '../../utils/htmlToElement';
+import { answerStatuses } from '../../data/statuses';
+
+import './styles.css';
+import answerButton from './index.html';
+
+export const AnswerButton = ({ title }) => {
+  const itemElement = htmlToElement(answerButton);
+  const titleElement = itemElement.querySelector('.answer-item__title');
+
+  titleElement.innerText = title;
+
+  return itemElement;
+};
+
+export const setStatus = (button, answerStatus) => {
+  if (answerStatus === answerStatuses.correct) {
+    button.classList.add('indicator_correct');
+  } else if (answerStatus === answerStatuses.wrong) {
+    button.classList.add('indicator_wrong');
+  }
+};
